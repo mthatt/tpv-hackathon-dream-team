3eba40f688bedeeccf5acf3e7088b276dbcbb51f
Edward Park <ed.sh.park@gmail.com>
2024-08-08 10:27:54 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import controlflow as cf
+
+classifier = cf.Agent(
+    name="Classifier",
+    description="An AI agent that classifies Google Calendar events to identify spam",
+    instructions="""
+        Your goal should be to classify Google Calendar events to identify if
+        they were created by a spammer or not. Be strict in your assessments.
+
+        only classify the event as spam if it is very likely to be spam based on these examples:
+
+        ```
+        SPAM EXAMPLES:
+
+        description: 3109889710
+        description: Cowbell
+        description: asdf
+        ```
+
+        ```
+        LEGIT EXAMPLES:
+
+        description: Orchestration
+        description: Im a data engineer
+        """,
+)
+
+@cf.flow
+def classify_calendar_event():
+  event = {}
+
+  is_spam = cf.Task(
+    "Classify the following calendar event to determine if it is submitted by a spammer",
+    result_type=bool,
+    context=dict(event=event),
+    agents=[classifier],
+  )
+
+
