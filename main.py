1daa6d396cae39bafc38e638923b31e4811349be
Edward Park <ed.sh.park@gmail.com>
2024-08-08 11:27:06 -0500

    fix classificaiton call

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import controlflow as cf
+from prefect import get_run_logger
+import json
+# from langchain_openai import ChatOpenAI
 
 classifier = cf.Agent(
     name="Classifier",
+    # model=ChatOpenAI(model_name="gpt-3.5-turbo"),
     description="An AI agent that classifies Google Calendar events to identify spam",
     instructions="""
         Your goal should be to classify Google Calendar events to identify if
@@ -22,12 +26,16 @@ classifier = cf.Agent(
 
         description: Orchestration
         description: Im a data engineer
+        ```
         """,
 )
 
 @cf.flow
-def classify_calendar_event():
-  event = {}
+def classify_calendar_event(event_json: str):
+  logger = get_run_logger()
+  logger.info(f"Classifying event: {event_json}")
+  # Event JSON schema: {"description": str, "created_date": str, "event_name": str, "invitee_email": str, "event_id": str}
+  event = json.loads(event_json)
 
   is_spam = cf.Task(
     "Classify the following calendar event to determine if it is submitted by a spammer",
@@ -36,4 +44,9 @@ def classify_calendar_event():
     agents=[classifier],
   )
 
+  logger.info(f"Is spam: {is_spam}")
 
+if __name__ == "__main__":
+  classify_calendar_event.serve(
+    name="classify-calendar-event",
+  )
